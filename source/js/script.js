99ff1d8ed91870e95764160383fae111cc072b5a
mircil <32936885+mircil@users.noreply.github.com>
2020-03-23 09:08:46 +0200

    Project done

---
+++ source/js/script.js
@@ -0,0 +1,26 @@
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var pageheader = document.querySelector('.page-header');
+var colorMenu = false;
+
+  navMain.classList.remove('main-nav--nojs');
+  navMain.classList.add('main-nav--closed');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--opened')) {
+      navMain.classList.remove('main-nav--opened');
+      navMain.classList.add('main-nav--closed');
+    } else {
+      navMain.classList.add('main-nav--opened');
+      navMain.classList.remove('main-nav--closed');
+    }
+    colorMenu = !colorMenu
+    console.log(colorMenu)
+    if(colorMenu) {
+      pageheader.classList.remove('page-header--close');
+      pageheader.classList.add('page-header--open');
+    } else {
+      pageheader.classList.remove('page-header--open');
+      pageheader.classList.add('page-header--close');
+    }
+  });
